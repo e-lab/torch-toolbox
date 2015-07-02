fa0003cf0c93e4144f88776e9fbc5a3bed391272
Jonghoon Jin <jhjin0@gmail.com>
2015-07-02 16:28:41 -0400

    Simplify BN-absorber routine and example
    Assuming BN layer comes right after convolution layer, its
    implementation is simpler and it does not cause numerical error.

--- BN-absorber/BN-absorber.lua
+++ BN-absorber/BN-absorber.lua
@@ -1,98 +1,62 @@
-local function manipulate_BN_spatial(model, i_cv, i_bn)
-   -- get pointers
-   local nb_input  = model.modules[i_cv].nInputPlane
-   local nb_output = model.modules[i_cv].nOutputPlane
-   local kH = model.modules[i_cv].kH
-   local kW = model.modules[i_cv].kW
-   local w = model.modules[i_cv].weight:view(nb_output, nb_input*kH*kW)
-   local b = model.modules[i_cv].bias
-   local mean = model.modules[i_bn].running_mean:view(nb_input, 1)
-   local std  = model.modules[i_bn].running_std:view(nb_input, 1)
+require('nn')
 
-   -- manipulate
-   w:cmul(torch.repeatTensor(std, nb_output, kH*kW))
-   b:addmv(w, -torch.repeatTensor(mean, 1, kH*kW):view(nb_input*kH*kW))
 
-   -- remove BN
-   model:remove(i_bn)
+local function absorb_bn(w, b, mean, std, affine, gamma, beta)
+   w:cmul(std:view(w:size(1),1):repeatTensor(1,w:size(2)))
+   b:add(-mean):cmul(std)
+
+   if affine then
+      w:cmul(gamma:view(w:size(1),1):repeatTensor(1,w:size(2)))
+      b:cmul(gamma):add(beta)
+   end
 end
 
 
-local function manipulate_BN_linear(model, i_cv, i_bn)
-   -- get pointers
-   local nb_input  = model.modules[i_cv].weight:size(2)
-   local nb_output = model.modules[i_cv].weight:size(1)
-   local w = model.modules[i_cv].weight
-   local b = model.modules[i_cv].bias
-   local mean = model.modules[i_bn].running_mean
-   local std  = model.modules[i_bn].running_std
+local function BN_absorber(x)
+   local i = 1
+   while (i <= #x.modules) do
+      if x.modules[i].__typename == 'nn.Sequential' then
+         BN_absorber(x.modules[i])
+      elseif x.modules[i].__typename == 'nn.Parallel' then
+         BN_absorber(x.modules[i])
+      elseif x.modules[i].__typename == 'nn.Concat' then
+         BN_absorber(x.modules[i])
+      elseif x.modules[i].__typename == 'nn.DataParallel' then
+         BN_absorber(x.modules[i])
+      elseif x.modules[i].__typename == 'nn.ModelParallel' then
+         BN_absorber(x.modules[i])
+      else
+         -- check BN
+         if x.modules[i].__typename == 'nn.SpatialBatchNormalization' or
+            x.modules[i].__typename == 'nn.BatchNormalization' then
+            if x.modules[i-1] and
+              (x.modules[i-1].__typename == 'nn.Linear' or
+               x.modules[i-1].__typename == 'nn.SpatialConvolution' or
+               x.modules[i-1].__typename == 'nn.SpatialConvolutionMM') then
+               -- force weight to be in 2-dim
+               local weight = x.modules[i-1].weight
+               weight = weight:view(weight:size(1), weight:nElement()/weight:size(1))
 
-   -- manipulate
-   w:cmul(torch.repeatTensor(std, nb_output, 1))
-   b:addmv(w, -mean)
-
-   -- remove BN
-   model:remove(i_bn)
-end
-
-
-local function BN_absorber(model_old, inplace)
-   local inplace = inplace or false
-   local model = (inplace and model_old) or model_old:clone()
-   local BN = {
-      index = 0,
-      spatial = true,
-      dim = 0,
-   }
-   local offset = 0
-
-   for i = 1, #model do
-      i = i - offset
-
-      -- (1) mark if BN
-      if model.modules[i].__typename == 'nn.SpatialBatchNormalization' then
-         BN.index = i
-         BN.spatial = true
-         BN.dim = model.modules[i].running_mean:nElement()
-      elseif model.modules[i].__typename == 'nn.BatchNormalization' then
-         BN.index = i
-         BN.spatial = false
-         BN.dim = model.modules[i].running_mean:nElement()
-
-
-      -- (2) absorb BN into CV
-      elseif (model.modules[i].__typename == 'nn.SpatialConvolution') or
-             (model.modules[i].__typename == 'nn.SpatialConvolutionMM') then
-         if (BN.index > 0) and BN.spatial and (BN.dim == model.modules[i].nInputPlane) then
-            manipulate_BN_spatial(model, i, BN.index)
-            BN.index = 0
-            offset = offset + 1
+               -- remove BN
+               absorb_bn(weight,
+                         x.modules[i-1].bias,
+                         x.modules[i].running_mean,
+                         x.modules[i].running_std,
+                         x.modules[i].affine,
+                         x.modules[i].weight,
+                         x.modules[i].bias)
+               x:remove(i)
+               i = i - 1
+            else
+               assert(false, 'Convolution module must exist right before batch normalization layer')
+            end
          end
-      elseif model.modules[i].__typename == 'nn.Linear' then
-         if (BN.index > 0) and not BN.spatial and (BN.dim == model.modules[i].weight:size(2)) then
-            manipulate_BN_linear(model, i, BN.index)
-            BN.index = 0
-            offset = offset + 1
-         end
-
-      -- (3) set behavior for others
-      elseif (model.modules[i].__typename == 'nn.SpatialMaxPooling') or
-             (model.modules[i].__typename == 'nn.Dropout') then
-         -- do nothing except Dropout v1
-      elseif (model.modules[i].__typename == 'nn.View') or
-             (model.modules[i].__typename == 'nn.Reshape') or
-             (model.modules[i].__typename == 'nn.ReLU') or
-             (model.modules[i].__typename == 'nn.Threshold') or
-             (model.modules[i].__typename == 'nn.SoftMax') or
-             (model.modules[i].__typename == 'nn.LogSoftMax') or
-             (model.modules[i].__typename == 'nn.SpatialAveragePooling') then
-         -- prev BN index not needed
-         BN.index = 0
       end
-      collectgarbage()
+      i = i + 1
    end
 
-   return model
+   collectgarbage()
+   return x
 end
 
 
--- BN-absorber/example.lua
+++ BN-absorber/example.lua
@@ -7,26 +7,28 @@ local x = torch.randn(2, 3, 5, 5)
 -- set dummy model
 local model = nn.Sequential()
 model:add(nn.SpatialConvolutionMM(3, 10, 3, 3))
+model:add(nn.SpatialBatchNormalization(10))
 model:add(nn.ReLU())
-model:add(nn.SpatialBatchNormalization(10, nil, nil, false))
 model:add(nn.SpatialConvolutionMM(10, 10, 3, 3))
+model:add(nn.SpatialBatchNormalization(10))
 model:add(nn.ReLU())
 model:add(nn.View(10))
-model:add(nn.BatchNormalization(10, nil, nil, false))
 model:add(nn.Linear(10, 10))
+model:add(nn.BatchNormalization(10))
 model:evaluate()
 
 
 -- assign non-trivial initial values to BN
-model.modules[3].running_mean:rand(10)
-model.modules[3].running_std:rand(10):abs()
-model.modules[7].running_mean:rand(10)
-model.modules[7].running_std:rand(10):abs()
+for _,i in ipairs{2, 5, 9} do
+   model.modules[i].running_mean:rand(10)
+   model.modules[i].running_std:rand(10):abs()
+   model.modules[i].weight:rand(10)
+   model.modules[i].bias:rand(10)
+end
 
 
 -- absorb batch normalization into convolution layer
-local inplace = false
-local model_wo_BN = require('BN-absorber')(model, inplace)
+local model_wo_BN = require('BN-absorber')(model:clone())
 
 
 -- evaluate each model
