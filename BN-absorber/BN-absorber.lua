ec286cd491f83e097bf8312f686828b1ffffbb7e
Jonghoon Jin <jhjin0@gmail.com>
2016-02-03 01:37:16 -0500

    BN absorber supports deconvolution module

--- BN-absorber/BN-absorber.lua
+++ BN-absorber/BN-absorber.lua
@@ -1,12 +1,23 @@
 require('nn')
 
 
-local function absorb_bn(w, b, mean, invstd, affine, gamma, beta)
-   w:cmul(invstd:view(w:size(1),1):repeatTensor(1,w:size(2)))
+local absorb_bn_conv = function (w, b, mean, invstd, affine, gamma, beta)
+   w:cmul(invstd:view(w:size(1),1):repeatTensor(1,w:nElement()/w:size(1)))
    b:add(-mean):cmul(invstd)
 
    if affine then
-      w:cmul(gamma:view(w:size(1),1):repeatTensor(1,w:size(2)))
+      w:cmul(gamma:view(w:size(1),1):repeatTensor(1,w:nElement()/w:size(1)))
+      b:cmul(gamma):add(beta)
+   end
+end
+
+
+local absorb_bn_deconv = function (w, b, mean, invstd, affine, gamma, beta)
+   w:cmul(invstd:view(b:size(1),1):repeatTensor(w:size(1),w:nElement()/w:size(1)/b:nElement()))
+   b:add(-mean):cmul(invstd)
+
+   if affine then
+      w:cmul(gamma:view(b:size(1),1):repeatTensor(w:size(1),w:nElement()/w:size(1)/b:nElement()))
       b:cmul(gamma):add(beta)
    end
 end
@@ -31,18 +42,24 @@ local function BN_absorber(x)
             if x.modules[i-1] and
               (x.modules[i-1].__typename == 'nn.SpatialConvolution' or
                x.modules[i-1].__typename == 'nn.SpatialConvolutionMM') then
-               -- force weight to be in 2-dim
-               local weight = x.modules[i-1].weight
-               weight = weight:view(weight:size(1), weight:nElement()/weight:size(1))
-
-               -- remove BN
-               absorb_bn(weight,
-                         x.modules[i-1].bias,
-                         x.modules[i].running_mean,
-                         x.modules[i].running_var:clone():sqrt():add(x.modules[i].eps):pow(-1),
-                         x.modules[i].affine,
-                         x.modules[i].weight,
-                         x.modules[i].bias)
+               absorb_bn_conv(x.modules[i-1].weight,
+                              x.modules[i-1].bias,
+                              x.modules[i].running_mean,
+                              x.modules[i].running_var:clone():sqrt():add(x.modules[i].eps):pow(-1),
+                              x.modules[i].affine,
+                              x.modules[i].weight,
+                              x.modules[i].bias)
+               x:remove(i)
+               i = i - 1
+            elseif x.modules[i-1] and
+                  (x.modules[i-1].__typename == 'nn.SpatialFullConvolution') then
+               absorb_bn_deconv(x.modules[i-1].weight,
+                                x.modules[i-1].bias,
+                                x.modules[i].running_mean,
+                                x.modules[i].running_var:clone():sqrt():add(x.modules[i].eps):pow(-1),
+                                x.modules[i].affine,
+                                x.modules[i].weight,
+                                x.modules[i].bias)
                x:remove(i)
                i = i - 1
             else
@@ -51,15 +68,13 @@ local function BN_absorber(x)
          elseif x.modules[i].__typename == 'nn.BatchNormalization' then
             if x.modules[i-1] and
               (x.modules[i-1].__typename == 'nn.Linear') then
-
-               -- remove BN
-               absorb_bn(x.modules[i-1].weight,
-                         x.modules[i-1].bias,
-                         x.modules[i].running_mean,
-                         x.modules[i].running_std,
-                         x.modules[i].affine,
-                         x.modules[i].weight,
-                         x.modules[i].bias)
+               absorb_bn_conv(x.modules[i-1].weight,
+                              x.modules[i-1].bias,
+                              x.modules[i].running_mean,
+                              x.modules[i].running_std,
+                              x.modules[i].affine,
+                              x.modules[i].weight,
+                              x.modules[i].bias)
                x:remove(i)
                i = i - 1
             else
